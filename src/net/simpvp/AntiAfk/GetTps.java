70c5a2ecf3f01008d4d851cb9aaff93a8bad486d
Laztec <110450926+Laztec@users.noreply.github.com>
2026-02-25 13:20:57 +0000

    Replace reflection with Paper API

--- src/net/simpvp/AntiAfk/GetTps.java
+++ src/net/simpvp/AntiAfk/GetTps.java
@@ -1,24 +1,9 @@
 package net.simpvp.AntiAfk;
 
-import java.lang.reflect.Field;
 import org.bukkit.Bukkit;
-import org.bukkit.Server;
 
 public class GetTps {
     public static double[] getTPS() {
-        Object server;
-        Field tps;
-        try {
-            Server mc = Bukkit.getServer();
-            Field consoleField = mc.getClass().getDeclaredField("console");
-            consoleField.setAccessible(true);
-            server = consoleField.get(mc);
-            tps = server.getClass().getSuperclass().getDeclaredField("recentTps");
-            tps.setAccessible(true);
-            return (double[]) tps.get(server);
-        } catch (IllegalAccessException | NoSuchFieldException ignored) {
-            AntiAfk.instance.getLogger().info("Error while getting tps");
-        }
-        return new double[]{20, 20, 20};
+        return Bukkit.getServer().getTPS();
     }
 }
